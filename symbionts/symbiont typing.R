741d4612eb766893b155afebf76484ea72dc9247
Michael-Studivan <studivanms@gmail.com>
2023-11-30 23:28:14 -0500

    zoox technical replicate comparison

--- symbionts/symbiont typing.R
+++ symbionts/symbiont typing.R
@@ -56,13 +56,15 @@ apply(wgsProp[, c(2:(ncol(wgsProp)))], 1, function(x) {
   sum(x, na.rm = T)
 })
 
+# cleaning up the sample ids
+wgsProp %>% mutate(across('Sample', str_replace, '.un.bt2.bam','')) -> wgsProp
+
 # exporting proportion data
 write.csv(wgsProp, file="WGS_zoox.csv")
 
 # transposing and reformatting dataframes to make abundance a single column
 wgsPerc <- reshape2::melt(wgsProp, id = "Sample")
 wgsPerc$variable=factor(wgsPerc$variable, levels=c("Symbiodinium","Breviolum","Cladocopium","Durusdinium")) 
-wgsPerc %>% mutate(across('Sample', str_replace, '.un.bt2.bam','')) -> wgsPerc
 
 # percent stacked barplot
 wgsPlot <- ggplot(wgsPerc, aes(fill=variable, y=value, x=Sample)) + 
@@ -70,10 +72,9 @@ wgsPlot <- ggplot(wgsPerc, aes(fill=variable, y=value, x=Sample)) +
   labs(x = "Sample",
        y = "Relative Abundance",
        fill = 'Symbiodiniaceae Genus',
-       title = "WGS (Experimental Genotypes") + 
+       title = "WGS (Experimental Genotypes)") + 
   scale_fill_manual(values = colorRampPalette(brewer.pal(8, "Accent"))(6)) +
   theme_classic() +
-  # geom_text(x = 1.8, y=1.035, label = "Site: F2,21 = 0.9, R2 = 0.085, p = ns") +
   theme(plot.title = element_text(hjust=0.5), legend.position = "bottom", axis.title.y = element_blank(), axis.text.y = element_blank(), axis.title.x = element_blank())
 wgsPlot 
 
@@ -136,14 +137,16 @@ apply(radProp[, c(2:(ncol(radProp)))], 1, function(x) {
   sum(x, na.rm = T)
 })
 
+# cleaning up the sample ids
+radProp %>% mutate(across('Sample', str_replace, '.trim.un.bt2.bam','')) %>%
+  mutate(across('Sample', str_replace, 'CPR_','')) -> radProp
+
 # exporting proportion data
 write.csv(radProp, file="2bRAD_zoox.csv")
 
 # transposing and reformatting dataframes to make abundance a single column
 radPerc <- reshape2::melt(radProp, id = "Sample")
 radPerc$variable=factor(radPerc$variable, levels=c("Symbiodinium","Breviolum","Cladocopium","Durusdinium")) 
-radPerc %>% mutate(across('Sample', str_replace, '.trim.un.bt2.bam','')) %>%
-  mutate(across('Sample', str_replace, 'CPR_','')) -> radPerc
 
 # percent stacked barplot
 radPlot <- ggplot(radPerc, aes(fill=variable, y=value, x=Sample)) + 
@@ -151,10 +154,9 @@ radPlot <- ggplot(radPerc, aes(fill=variable, y=value, x=Sample)) +
   labs(x = "Sample",
        y = "Relative Abundance",
        fill = 'Symbiodiniaceae Genus',
-       title = "2bRAD (Experimental Genotypes") + 
+       title = "2bRAD (Experimental Genotypes)") + 
   scale_fill_manual(values = colorRampPalette(brewer.pal(8, "Accent"))(6)) +
   theme_classic() +
-  # geom_text(x = 1.8, y=1.035, label = "Site: F2,21 = 0.9, R2 = 0.085, p = ns") +
   theme(plot.title = element_text(hjust=0.5), legend.position = "bottom", axis.title.y = element_blank(), axis.text.y = element_blank(), axis.title.x = element_blank())
 radPlot 
 
@@ -165,3 +167,48 @@ radPerc %>%
   group_by(variable) %>%
   dplyr::summarize(Mean = mean(value, na.rm=TRUE)) -> radMean
 write.csv(radMean, file = "2bRAD_zoox_means.csv")
+
+
+#### combined genotypes ####
+
+genos <- read.csv("genotype_lookup.csv", head=T)
+
+zooxPerc <- rbind (wgsPerc, radPerc)
+zooxPerc %>%
+  left_join(genos, by = "Sample") -> zooxPerc
+
+# percent stacked barplot
+zooxPlot <- ggplot(zooxPerc, aes(fill=variable, y=value, x=GenoRep)) + 
+  geom_bar(position="fill", stat="identity") +
+  labs(x = "Putative Genotype",
+       y = "Relative Abundance",
+       fill = 'Symbiodiniaceae Genus',
+       title = "All Genotypes") + 
+  scale_fill_manual(values = colorRampPalette(brewer.pal(8, "Accent"))(6)) +
+  theme_classic() +
+  facet_grid(~PutGeno, scales = "free_x", space = "free") +
+  theme(plot.title = element_text(hjust=0.5), legend.position = "bottom", axis.title.y = element_blank(), axis.text.y = element_blank(), axis.title.x = element_blank())
+zooxPlot 
+
+ggsave("All symbionts.pdf", plot= zooxPlot, width=172, height=4, units="in", dpi=300, limitsize = F)
+
+
+#### technical replicates ####
+
+zooxPerc %>%
+  filter(Reps == "yes") -> zooxReps
+
+# percent stacked barplot
+repsPlot <- ggplot(zooxReps, aes(fill=variable, y=value, x=GenoRep)) + 
+  geom_bar(position="fill", stat="identity") +
+  labs(x = "Putative Genotype",
+       y = "Relative Abundance",
+       fill = 'Symbiodiniaceae Genus',
+       title = "Technical Replicates") + 
+  scale_fill_manual(values = colorRampPalette(brewer.pal(8, "Accent"))(6)) +
+  theme_classic() +
+  facet_grid(~PutGeno, scales = "free_x", space = "free") +
+  theme(plot.title = element_text(hjust=0.5), legend.position = "bottom", axis.title.y = element_blank(), axis.text.y = element_blank(), axis.title.x = element_blank())
+repsPlot 
+
+ggsave("Technical replicates.pdf", plot= repsPlot, width=64, height=4, units="in", dpi=300, limitsize = F)
